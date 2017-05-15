743fb8f0fc3bb3a0e3ad9db59e0fc986d34a3447
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-05-15 17:11:35 +0300

    test: fix keepAliveTimeout tests
    * Don't use `common.PORT` in parallel tests.
    * Don't try to connect to a server until the 'listening' event is fired.
    * Fix a typo in the name of a test.
    * Use arrow functions.

--- test/parallel/test-http-server-keep-alive-timeout.js
+++ test/parallel/test-http-server-keep-alive-timeout.js
@@ -24,32 +24,37 @@ test(function serverEndKeepAliveTimeoutWithPipeline(cb) {
   let destroyedSockets = 0;
   let timeoutCount = 0;
   let requestCount = 0;
-  process.on('exit', function() {
+  process.on('exit', () => {
     assert.strictEqual(timeoutCount, 1);
     assert.strictEqual(requestCount, 3);
     assert.strictEqual(destroyedSockets, 1);
   });
-  const server = http.createServer(function(req, res) {
+  const server = http.createServer((req, res) => {
     socket = req.socket;
     requestCount++;
     res.end();
   });
-  server.setTimeout(100, function(socket) {
+  server.setTimeout(100, (socket) => {
     timeoutCount++;
     socket.destroy();
   });
   server.keepAliveTimeout = 50;
-  server.listen(common.PORT);
-  const c = net.connect({ port: common.PORT, allowHalfOpen: true }, function() {
-    c.write('GET /1 HTTP/1.1\r\nHost: localhost\r\n\r\n');
-    c.write('GET /2 HTTP/1.1\r\nHost: localhost\r\n\r\n');
-    c.write('GET /3 HTTP/1.1\r\nHost: localhost\r\n\r\n');
-  });
-  setTimeout(function() {
-    server.close();
-    if (socket.destroyed) destroyedSockets++;
-    cb();
-  }, 1000);
+  server.listen(0, common.mustCall(() => {
+    const options = {
+      port: server.address().port,
+      allowHalfOpen: true
+    };
+    const c = net.connect(options, () => {
+      c.write('GET /1 HTTP/1.1\r\nHost: localhost\r\n\r\n');
+      c.write('GET /2 HTTP/1.1\r\nHost: localhost\r\n\r\n');
+      c.write('GET /3 HTTP/1.1\r\nHost: localhost\r\n\r\n');
+    });
+    setTimeout(() => {
+      server.close();
+      if (socket.destroyed) destroyedSockets++;
+      cb();
+    }, 1000);
+  }));
 });
 
 test(function serverNoEndKeepAliveTimeoutWithPipeline(cb) {
@@ -57,29 +62,34 @@ test(function serverNoEndKeepAliveTimeoutWithPipeline(cb) {
   let destroyedSockets = 0;
   let timeoutCount = 0;
   let requestCount = 0;
-  process.on('exit', function() {
+  process.on('exit', () => {
     assert.strictEqual(timeoutCount, 1);
     assert.strictEqual(requestCount, 3);
     assert.strictEqual(destroyedSockets, 1);
   });
-  const server = http.createServer(function(req, res) {
+  const server = http.createServer((req, res) => {
     socket = req.socket;
     requestCount++;
   });
-  server.setTimeout(100, function(socket) {
+  server.setTimeout(100, (socket) => {
     timeoutCount++;
     socket.destroy();
   });
   server.keepAliveTimeout = 50;
-  server.listen(common.PORT);
-  const c = net.connect({ port: common.PORT, allowHalfOpen: true }, function() {
-    c.write('GET /1 HTTP/1.1\r\nHost: localhost\r\n\r\n');
-    c.write('GET /2 HTTP/1.1\r\nHost: localhost\r\n\r\n');
-    c.write('GET /3 HTTP/1.1\r\nHost: localhost\r\n\r\n');
-  });
-  setTimeout(function() {
-    server.close();
-    if (socket.destroyed) destroyedSockets++;
-    cb();
-  }, 1000);
+  server.listen(0, common.mustCall(() => {
+    const options = {
+      port: server.address().port,
+      allowHalfOpen: true
+    };
+    const c = net.connect(options, () => {
+      c.write('GET /1 HTTP/1.1\r\nHost: localhost\r\n\r\n');
+      c.write('GET /2 HTTP/1.1\r\nHost: localhost\r\n\r\n');
+      c.write('GET /3 HTTP/1.1\r\nHost: localhost\r\n\r\n');
+    });
+    setTimeout(() => {
+      server.close();
+      if (socket.destroyed) destroyedSockets++;
+      cb();
+    }, 1000);
+  }));
 });
--- test/parallel/test-https-server-keep-alive-timeout.js
+++ test/parallel/test-https-server-keep-alive-timeout.js
@@ -35,67 +35,69 @@ test(function serverKeepAliveTimeoutWithPipeline(cb) {
     assert.strictEqual(requestCount, 3);
     assert.strictEqual(destroyedSockets, 1);
   });
-  const server = https.createServer(serverOptions, function(req, res) {
+  const server = https.createServer(serverOptions, (req, res) => {
     socket = req.socket;
     requestCount++;
     res.end();
   });
-  server.setTimeout(100, function(socket) {
+  server.setTimeout(100, (socket) => {
     timeoutCount++;
     socket.destroy();
   });
   server.keepAliveTimeout = 50;
-  server.listen(common.PORT);
-  const options = {
-    port: common.PORT,
-    allowHalfOpen: true,
-    rejectUnauthorized: false
-  };
-  const c = tls.connect(options, function() {
-    c.write('GET /1 HTTP/1.1\r\nHost: localhost\r\n\r\n');
-    c.write('GET /2 HTTP/1.1\r\nHost: localhost\r\n\r\n');
-    c.write('GET /3 HTTP/1.1\r\nHost: localhost\r\n\r\n');
-  });
-  setTimeout(function() {
-    server.close();
-    if (socket.destroyed) destroyedSockets++;
-    cb();
-  }, 1000);
+  server.listen(0, common.mustCall(() => {
+    const options = {
+      port: server.address().port,
+      allowHalfOpen: true,
+      rejectUnauthorized: false
+    };
+    const c = tls.connect(options, () => {
+      c.write('GET /1 HTTP/1.1\r\nHost: localhost\r\n\r\n');
+      c.write('GET /2 HTTP/1.1\r\nHost: localhost\r\n\r\n');
+      c.write('GET /3 HTTP/1.1\r\nHost: localhost\r\n\r\n');
+    });
+    setTimeout(() => {
+      server.close();
+      if (socket.destroyed) destroyedSockets++;
+      cb();
+    }, 1000);
+  }));
 });
 
-test(function serverNoKeepAliveTimeoutWithPipeline(cb) {
+test(function serverNoEndKeepAliveTimeoutWithPipeline(cb) {
   let socket;
   let destroyedSockets = 0;
   let timeoutCount = 0;
   let requestCount = 0;
-  process.on('exit', function() {
+  process.on('exit', () => {
     assert.strictEqual(timeoutCount, 1);
     assert.strictEqual(requestCount, 3);
     assert.strictEqual(destroyedSockets, 1);
   });
-  const server = https.createServer(serverOptions, function(req, res) {
+  const server = https.createServer(serverOptions, (req, res) => {
     socket = req.socket;
     requestCount++;
   });
-  server.setTimeout(100, function(socket) {
+  server.setTimeout(100, (socket) => {
     timeoutCount++;
     socket.destroy();
   });
   server.keepAliveTimeout = 50;
-  server.listen(common.PORT);
-  const options = {
-    port: common.PORT,
-    allowHalfOpen: true,
-    rejectUnauthorized: false
-  };
-  const c = tls.connect(options, function() {
-    c.write('GET /1 HTTP/1.1\r\nHost: localhost\r\n\r\n');
-    c.write('GET /2 HTTP/1.1\r\nHost: localhost\r\n\r\n');
-    c.write('GET /3 HTTP/1.1\r\nHost: localhost\r\n\r\n');
-  });
-  setTimeout(function() {
-    server.close();
-    if (socket && socket.destroyed) destroyedSockets++;
-    cb();
-  }, 1000);
+  server.listen(0, common.mustCall(() => {
+    const options = {
+      port: server.address().port,
+      allowHalfOpen: true,
+      rejectUnauthorized: false
+    };
+    const c = tls.connect(options, () => {
+      c.write('GET /1 HTTP/1.1\r\nHost: localhost\r\n\r\n');
+      c.write('GET /2 HTTP/1.1\r\nHost: localhost\r\n\r\n');
+      c.write('GET /3 HTTP/1.1\r\nHost: localhost\r\n\r\n');
+    });
+    setTimeout(() => {
+      server.close();
+      if (socket && socket.destroyed) destroyedSockets++;
+      cb();
+    }, 1000);
+  }));
 });
